1aaa580d291251a17c74383ad262ea2f37f448ce
kvos64 <kollinray64@gmail.com>
2025-02-02 11:06:14 -0600

    Go Mmsu Bears

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,23 +11,11 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-        trueButton.setOnClickListener { _: View ->
-            // Do something in response to the click here
-
-                Snackbar.make(trueButton, "You are Correct!", Snackbar.LENGTH_LONG).show()
-        }
-        falseButton.setOnClickListener { _: View ->
-            // Do something in response to the click here
-            Snackbar.make(falseButton, "You are Incorrect!", Snackbar.LENGTH_LONG).show()
-        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
