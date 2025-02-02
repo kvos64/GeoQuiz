27c9f636e79acc98300a8a8620a391b4511a412a
kvos64 <kollinray64@gmail.com>
2025-02-01 23:40:27 -0600

    added Snackbar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -20,19 +21,12 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         trueButton.setOnClickListener { _: View ->
             // Do something in response to the click here
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+
+                Snackbar.make(trueButton, "You are Correct!", Snackbar.LENGTH_LONG).show()
         }
         falseButton.setOnClickListener { _: View ->
             // Do something in response to the click here
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+            Snackbar.make(falseButton, "You are Incorrect!", Snackbar.LENGTH_LONG).show()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -41,5 +35,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
 }
+
 // commit 1 code
